614e532b7796d0f33eef53b4524f07def1bff21b
신희성 <vvwv3092@gmail.com>
2024-01-11 23:17:11 +0900

    init

---
+++ src/main/java/com/example/ouruniverse/OuruniverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ouruniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuruniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuruniverseApplication.class, args);
+    }
+
+}
