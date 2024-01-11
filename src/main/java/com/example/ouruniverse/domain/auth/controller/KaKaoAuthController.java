79149d87f0c6d047333714733314a0a4a473ad0e
신희성 <vvwv3092@gmail.com>
2024-01-12 00:29:20 +0900

    Add login api

--- src/main/java/com/example/ouruniverse/domain/auth/controller/KaKaoAuthController.java
+++ src/main/java/com/example/ouruniverse/domain/auth/controller/KaKaoAuthController.java
@@ -4,9 +4,14 @@ import com.example.ouruniverse.domain.auth.controller.dto.KaKaoAccount;
 import com.example.ouruniverse.domain.auth.service.KaKaoAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +20,35 @@ public class KaKaoAuthController {
 
     private final KaKaoAuthService kaKaoAuthService;
 
+
+    @Value("${kakao.auth-url}")
+    private String kakaoAuthUrl;
+
+    @Value("${kakao.user-api-url}")
+    private String kakaoUserApiUrl;
+
+    @Value("${kakao.restapi-key}")
+    private String restapiKey;
+
+    @Value("${kakao.redirect-url}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public KaKaoAccount getKakaoAccount(@RequestParam("code") String code) {
       log.debug("code = {}", code);
       return kaKaoAuthService.getInfo(code).getKakaoAccount();
     }
 
+    @GetMapping("/login")
+    public ResponseEntity<Void> KaKaoAuthLink() {
+        String redirectUrl = "https://kauth.kakao.com/oauth/authorize" +
+                "?response_type=code&" +
+                "client_id=" + restapiKey +
+                "&redirect_uri=" + redirectUri;
+
+        return ResponseEntity
+                .status(302)
+                .location(UriComponentsBuilder.fromHttpUrl(redirectUrl).build().toUri())
+                .build();
+    }
 }
